e0d0d86c582e0107cb2d54eb79f65c747b578ff1
Lance Albertson <lance@osuosl.org>
2019-06-22 17:35:32 -0700

    Add ip6tables resource
    This should resolve #1011 which provides an ip6tables resource to test IPv6
    iptables rules. This is essentially a copy of the iptables resource with a few
    renames.
    
    In addition, I've pulled in the integration tests for iptables into ip6tables
    and enabled it on docker so that it properly gets tested regularly. The test
    cookbook recipe has been updated to support all of the current platforms that
    are being tested.
    
    Signed-off-by: Lance Albertson <lance@osuosl.org>

--- test/cookbooks/os_prepare/recipes/iptables.rb
+++ test/cookbooks/os_prepare/recipes/iptables.rb
@@ -1,6 +1,7 @@
 # author: Stephan Renatus
 
 if platform_family?('rhel', 'debian', 'fedora')
+  # IPv4
   execute 'iptables -A INPUT -i eth0 -p tcp -m tcp '\
           '--dport 80 -m state --state NEW -m comment '\
           '--comment "http on 80" -j ACCEPT'
@@ -8,4 +9,12 @@ if platform_family?('rhel', 'debian', 'fedora')
   execute 'iptables -A INPUT -j derby-cognos-web'
   execute 'iptables -A derby-cognos-web -p tcp -m tcp --dport 80 '\
           '-m comment --comment "derby-cognos-web" -j ACCEPT'
+  # IPv6
+  execute "ip6tables -A INPUT -i eth0 -p tcp -m tcp "\
+          "--dport 80 -m state --state NEW -m comment "\
+          '--comment "http v6 on 80" -j ACCEPT'
+  execute "ip6tables -N derby-cognos-web-v6"
+  execute "ip6tables -A INPUT -j derby-cognos-web-v6"
+  execute "ip6tables -A derby-cognos-web-v6 -p tcp -m tcp --dport 80 "\
+          '-m comment --comment "derby-cognos-web-v6" -j ACCEPT'
 end
