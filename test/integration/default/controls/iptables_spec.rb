587b43b60de05a3e9d8bb1bf799e1adc59c2d696
Lance Albertson <lance@osuosl.org>
2019-06-22 17:35:32 -0700

    Add ip6tables resource
    This should resolve #1011 which provides an ip6tables resource to test IPv6
    iptables rules. This is essentially a copy of the iptables resource with a few
    renames.
    
    In addition, I've pulled in the integration tests for iptables into ip6tables
    and enabled it on docker so that it properly gets tested regularly. The test
    cookbook recipe has been updated to support all of the current platforms that
    are being tested.
    
    Signed-off-by: Lance Albertson <lance@osuosl.org>

--- test/integration/default/controls/iptables_spec.rb
+++ test/integration/default/controls/iptables_spec.rb
@@ -5,7 +5,7 @@ if ENV['DOCKER']
 end
 
 case os[:family]
-when 'ubuntu', 'fedora'
+when 'ubuntu', 'fedora', 'debian', 'suse'
   describe iptables do
     it { should have_rule('-A INPUT -i eth0 -p tcp -m tcp --dport 80 -m state --state NEW -m comment --comment "http on 80" -j ACCEPT') }
     it { should_not have_rule('-A INPUT -i eth1 -p tcp -m tcp --dport 80 -j ACCEPT') }
@@ -13,7 +13,7 @@ when 'ubuntu', 'fedora'
     # single-word comments have their quotes dropped
     it { should have_rule('-A derby-cognos-web -p tcp -m tcp --dport 80 -m comment --comment derby-cognos-web -j ACCEPT') }
   end
-when  'rhel', 'centos'
+when  'redhat', 'centos'
   describe iptables do
     it { should have_rule('-A INPUT -i eth0 -p tcp -m tcp --dport 80 -m state --state NEW -m comment --comment "http on 80" -j ACCEPT') }
     it { should_not have_rule('-A INPUT -i eth1 -p tcp -m tcp --dport 80 -j ACCEPT') }
